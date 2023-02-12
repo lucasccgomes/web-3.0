961d50a37d51e2e4131b213db6f43a86a205e13e
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-02-12 09:16:22 -0300

    update

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,13 +1,10 @@
-import React, { useContext } from "react";
+
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
-
-import { TransactionContext } from "../context/TransactionContext";
-import { shortenAddress } from "../utils/shortenAddress";
 import { Loader } from "./";
 
-const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+const CommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 const Input = ({ placeholder, name, type, value, handleChange }) => (
   <input
@@ -21,16 +18,13 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-  const { currentAccount, connectWallet, handleChange, sendTransaction, formData, isLoading } = useContext(TransactionContext);
+  
+  const connectWallet = () => {
 
-  const handleSubmit = (e) => {
-    const { addressTo, amount, keyword, message } = formData;
+  }
 
-    e.preventDefault();
-
-    if (!addressTo || !amount || !keyword || !message) return;
-
-    sendTransaction();
+  const handleSubmit = () => {
+    
   };
 
   return (
@@ -43,57 +37,57 @@ const Welcome = () => {
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
             Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
           </p>
-          {!currentAccount && (
+          
             <button
               type="button"
               onClick={connectWallet}
               className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
             >
-              <AiFillPlayCircle className="text-white mr-2" />
+              
               <p className="text-white text-base font-semibold">
                 Connect Wallet
-              </p>
-            </button>
-          )}
+              //</p>
+          </button>
+        
 
-          <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
-            <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
+         <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+          <div className={`rounded-tl-2xl ${CommonStyles}`}>
               Reliability
-            </div>
-            <div className={companyCommonStyles}>Security</div>
-            <div className={`sm:rounded-tr-2xl ${companyCommonStyles}`}>
+           </div>
+            <div className={CommonStyles}>Security</div>
+            <div className={`sm:rounded-tr-2xl ${CommonStyles}`}>
               Ethereum
             </div>
-            <div className={`sm:rounded-bl-2xl ${companyCommonStyles}`}>
+            <div className={`sm:rounded-bl-2xl ${CommonStyles}`}>
               Web 3.0
             </div>
-            <div className={companyCommonStyles}>Low Fees</div>
-            <div className={`rounded-br-2xl ${companyCommonStyles}`}>
+            <div className={CommonStyles}>Low Fees</div>
+            <div className={`rounded-br-2xl ${CommonStyles}`}>
               Blockchain
-            </div>
+         </div>
           </div>
         </div>
 
         <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
           <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
             <div className="flex justify-between flex-col w-full h-full">
-              <div className="flex justify-between items-start">
+             <div className="flex justify-between items-start">
                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
-                  <SiEthereum fontSize={21} color="#fff" />
-                </div>
-                <BsInfoCircle fontSize={17} color="#fff" />
+                 <SiEthereum fontSize={21} color="#fff" />
+               </div>
+               <BsInfoCircle fontSize={17} color="#fff" />
               </div>
-              <div>
+            <div>
                 <p className="text-white font-light text-sm">
-                  {shortenAddress(currentAccount)}
-                </p>
-                <p className="text-white font-semibold text-lg mt-1">
+               Address
+               </p>
+              <p className="text-white font-semibold text-lg mt-1">
                   Ethereum
-                </p>
-              </div>
-            </div>
+              </p>
+             </div>
           </div>
-          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+        </div>
+         <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
             <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
             <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
             <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
@@ -101,9 +95,9 @@ const Welcome = () => {
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-            {isLoading
-              ? <Loader />
-              : (
+            {flase ? (
+               <Loader />
+               ) : (
                 <button
                   type="button"
                   onClick={handleSubmit}
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAdress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
