9d5e2a85e984c2379806938a1fc4aa27a80300e4
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-02-15 11:52:39 -0300

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 
-
 const App = () => (
   <div className="min-h-screen">
     <div className="gradient-bg-welcome">
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
-import abi from './Transactions.json';
+import abi from "./Transactions.json";
 
+export const contractAddress = "0xED994633CC97C242Bc9ba419C5b068eb10aC7cCa";
 export const contractABI = abi.abi;
-export const contractAddress = '0xED994633CC97C242Bc9ba419C5b068eb10aC7cCa';
